d3ff486dd0b4af3a87a49aa0aeed30d8d819a08d
InnaSaienko <inna.saienko.nl@gamil.com>
2025-07-25 15:36:40 +0200

    refactor: update Layout component to use Outlet and Suspense

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import Header from "../Header/Header.jsx";
+import Footer from "../Footer/Footer.jsx";
+import Loader from "../Loader/Loader.jsx";
+import {Outlet} from "react-router-dom";
 
 const Layout = () => {
-  return (
-    <div>Layout</div>
-  )
+    return (
+        <>
+            <Header/>
+            <Suspense fallback={<Loader />}>
+                <Outlet />
+            </Suspense>
+            <Footer/>
+        </>)
 }
 
 export default Layout
