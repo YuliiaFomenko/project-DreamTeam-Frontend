01dcabb85c71253d787134d17d735b0cd4a4bc0e
InnaSaienko <inna.saienko.nl@gamil.com>
2025-07-28 12:07:04 +0200

    added Toaster after Footer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,15 +3,17 @@ import Header from "../Header/Header.jsx";
 import Footer from "../Footer/Footer.jsx";
 import Loader from "../Loader/Loader.jsx";
 import {Outlet} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
 
 const Layout = () => {
     return (
         <>
             <Header/>
-            <Suspense fallback={<Loader />}>
-                <Outlet />
+            <Suspense fallback={<Loader/>}>
+                <Outlet/>
             </Suspense>
             <Footer/>
+            <Toaster position='top-right' reverseOrder={false}/>)
         </>)
 }
 
