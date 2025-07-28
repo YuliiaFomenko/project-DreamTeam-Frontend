7a5ee1e2b5e03cf52ede4a12c3a03f9aa086ca82
Inna Saienko <152598392+InnaSaienko@users.noreply.github.com>
2025-07-28 14:46:41 +0200

    App + Routes (#1)
    * added routes according to technical description
    
    * refactor routes to the component RouterSet
    
    * refactor: update Layout component to use Outlet and Suspense
    
    * added Toaster after Footer
    
    * refactored passed arg as only children
    
    * remove / from routes
    
    * update App return value Loader instead of null
    
    * add route for NotFoundPage
    
    ---------
    
    Co-authored-by: InnaSaienko <inna.saienko.nl@gamil.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectIsRefreshing } from "./redux/auth/selectors";
 import { refreshThunk } from "./redux/auth/operations";
-import { Routes } from "react-router-dom";
-import Header from "./components/Header/Header";
+import Routes from "./components/Routes/RouterSet.jsx";
+import Loader from "./components/Loader/Loader.jsx";
 
 
 const App = () => {
@@ -14,11 +14,6 @@ const App = () => {
     dispatch(refreshThunk());
   }, [dispatch]);
 
-  return isRefreshing ? null : (
-    <div>
-     <Routes>
-     </Routes>
-    </div>
-  );
+  return isRefreshing ? <Loader />: <Routes />
 };
 export default App;
