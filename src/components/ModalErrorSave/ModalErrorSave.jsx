69efbfb5e25689bc9caa9ca95dbcf5254924e1f1
OlexandrBolotov <boltik4a9@gmail.com>
2025-08-01 01:45:52 +0300

    Modal error save pull (#6)
    * ModalFirstCom
    
    * 2 commit

--- src/components/ModalErrorSave/ModalErrorSave.jsx
+++ src/components/ModalErrorSave/ModalErrorSave.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { ModalBase } from '../ModalBase/ModalBase';
+import styles from './ModalErrorSave.module.css';
+import { useNavigate } from 'react-router-dom';
 
-const ModalErrorSave = () => {
+export const ModalErrorSave = ({ onClose }) => {
+  const navigate = useNavigate();
+
   return (
-    <div>ModalErrorSave</div>
-  )
-}
-
-export default ModalErrorSave
+    <ModalBase onClose={onClose}>
+      <h2 className={styles.title}>Error while saving</h2>
+      <p className={styles.text}>
+        To save this article, you need to authorize first
+      </p>
+      <div className={styles.buttonGroup}>
+        <button
+          className={styles.loginBtn}
+          onClick={() => {
+            navigate('/login');
+            onClose();
+          }}
+        >
+          Login
+        </button>
+        <button
+          className={styles.registerBtn}
+          onClick={() => {
+            navigate('/register');
+            onClose();
+          }}
+        >
+          Register
+        </button>
+      </div>
+    </ModalBase>
+  );
+};
