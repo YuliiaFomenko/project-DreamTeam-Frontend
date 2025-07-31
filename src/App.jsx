1ced87311fe05b96b7be7fc0f882f6fc1de04472
maf1ck <kosmanmatvij@gmail.com>
2025-07-31 22:09:17 +0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { refreshThunk } from "./redux/auth/operations";
 import Routes from "./components/Routes/RouterSet.jsx";
 import Loader from "./components/Loader/Loader.jsx";
 import { UploadPhoto } from "./components/UploadPhoto/UploadPhoto.jsx";
+import { Route } from "react-router-dom";
 
 
 const App = () => {
@@ -15,6 +16,6 @@ const App = () => {
     dispatch(refreshThunk());
   }, [dispatch]);
 
-  return isRefreshing ? <Loader />: <UploadPhoto></UploadPhoto>
+  return isRefreshing ? <Loader />: <Route></Route>
 };
 export default App;
