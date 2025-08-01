b83058ffd4f7d0d301a03e2af4ae7a649ced243b
Inna Saienko <152598392+InnaSaienko@users.noreply.github.com>
2025-08-01 19:03:27 +0200

    create Loader component (#7)
    * create Loader component
    
    * refactor(loader): remove unnecessary useMemo
    
    ---------
    
    Co-authored-by: InnaSaienko <inna.saienko.nl@gamil.com>
    Co-authored-by: Yuliia <yaulia3@gmail.com>

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -5,3 +5,4 @@ export const selectRegistrationStatus = (state) =>
 export const selectToken = (state) => state.auth.token;
 export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
 export const selectIsRefreshing = (state) => state.auth.isRefreshing;
+
