0d20604d24799b6b9dcd9a17f71270d5ff4fe2d7
Miosha Tala <miosha.tala@gmail.com>
2025-08-01 13:02:47 +0300

    redux update 01

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -10,45 +10,56 @@ function setAuthHeader(token) {
 }
 
 const clearAuthHeader = () => {
-  goitAPI.defaults.headers.common.Authorization = '';
-}
+  goitAPI.defaults.headers.common.Authorization = "";
+};
 
-export const registerThunk = createAsyncThunk('auth/register', async (body, thunkAPI)=> {
-  try {
-    const response = await goitAPI.post('/auth/register', body);
-    setAuthHeader(response.data.token);
-    return response.data;
-  } catch (error){
-    return thunkAPI. rejectWithValue(error.message);
+export const registerThunk = createAsyncThunk(
+  "auth/register",
+  async (body, thunkAPI) => {
+    try {
+      const response = await goitAPI.post("/auth/register", body);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
-})
+);
 
-export const logInThunk = createAsyncThunk('auth/login', async (body, thunkAPI)=>{
-  try{
-    const response = await goitAPI.post('/auth/login', body)
-    setAuthHeader(response.data.accessToken)
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message)
+export const logInThunk = createAsyncThunk(
+  "auth/login",
+  async (body, thunkAPI) => {
+    try {
+      const response = await goitAPI.post("/auth/login", body);
+      setAuthHeader(response.data.accessToken);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
-})
+);
 
-export const logOutThunk = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
-  try {
-    await goitAPI.post("/auth/logout");
-    clearAuthHeader()
-  } catch (error){
-    return thunkAPI.rejectWithValue(error.message)
+export const logOutThunk = createAsyncThunk(
+  "auth/logout",
+  async (_, thunkAPI) => {
+    try {
+      await goitAPI.post("/auth/logout");
+      clearAuthHeader();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
-})
+);
 
-export const refreshThunk =createAsyncThunk('auth/refresh', async (_, thunkAPI)=>{
-  try {
-    const response = await goitAPI.get("/auth/refresh");
-    const accessToken = response.data.accessToken;
-    setAuthHeader(accessToken)
-    return response.data
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.message)
+export const refreshThunk = createAsyncThunk(
+  "auth/refresh",
+  async (_, thunkAPI) => {
+    try {
+      const response = await goitAPI.get("/auth/refresh");
+      const accessToken = response.data.accessToken;
+      setAuthHeader(accessToken);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
   }
-})
+);
