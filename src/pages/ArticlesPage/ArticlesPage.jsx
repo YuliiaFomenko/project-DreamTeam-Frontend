1092c1cf0f6e9dd81b7a971fe240c78f20fd9345
Miosha Tala <miosha.tala@gmail.com>
2025-08-02 18:32:30 +0300

    playing with cookies

--- src/pages/ArticlesPage/ArticlesPage.jsx
+++ src/pages/ArticlesPage/ArticlesPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useDispatch } from "react-redux";
+import { addToSaved } from "../../redux/user/operations.js";
+import { refreshThunk } from "../../redux/auth/operations.js";
 
 const ArticlesPage = () => {
+  const dispatch = useDispatch();
   return (
-    <div>ArticlesPage</div>
-  )
-}
+    <>
+      <div>ArticlesPage</div>
+      <button
+        onClick={() => {
+          dispatch(refreshThunk());
+          //dispatch(addToSaved("68498236a100312bea078fea"));
+        }}
+      >
+        Click Me
+      </button>
+    </>
+  );
+};
 
-export default ArticlesPage
+export default ArticlesPage;
--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -7,23 +7,23 @@ export const goitAPI = axios.create({
 }); //створення індивідуального екземпляра axios, який вже налаштований з baseURL
 
 function setAuthHeader(token) {
-  //goitAPI.defaults.headers.common.Authorization = `Bearer ${token}`;
-  goitAPI.interceptors.request.use(
-    (config) => {
-      if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
-    }
-  );
+  goitAPI.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  // goitAPI.interceptors.request.use(
+  //   (config) => {
+  //     if (token) {
+  //       config.headers.Authorization = `Bearer ${token}`;
+  //     }
+  //     return config;
+  //   },
+  //   (error) => {
+  //     return Promise.reject(error);
+  //   }
+  // );
 }
 
 const clearAuthHeader = () => {
-  //goitAPI.defaults.headers.common.Authorization = "";
-  delete goitAPI.defaults.headers.common["Authorization"];
+  goitAPI.defaults.headers.common["Authorization"] = "";
+  //delete goitAPI.defaults.headers.common["Authorization"];
 };
 
 export const registerThunk = createAsyncThunk(
