510f355ff305ab15a9347572dbe37a0873afa6a4
Phill Maslo <phillmaslo@gmail.com>
2025-08-02 17:25:08 +0200

    WIP: updated Creators

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import sprite from "../../assets/img/sprite.svg";
 import { Link } from 'react-router-dom';
 import css from './Footer.module.css';
-import logo from '../../assets/img/footer/logo-_1x_360.png';
-import logoFull from '../../assets/img/footer/logo-_2x_360.png';
+// import logo from '../../assets/img/footer/logo-_1x_360.png';
+// import logoFull from '../../assets/img/footer/logo-_2x_360.png';
 
 const Footer = () => {
   return (
@@ -11,14 +11,9 @@ const Footer = () => {
       <div className='container'>
         <div className={css.inner}>
         <Link to="/" className={css.logo}>
-          <img
-            src={logo}
-            srcSet={`${logo} 1x, ${logoFull} 2x`}
-            alt="Harmoniq Logo"
-            className={css.logoImg}
-            width="165"
-            height="46"
-          />
+          <svg width="165" height="46" className={css.logo} stroke="var(--green)">
+            <use href={`${sprite}#icon-logo`} />
+          </svg>
         </Link>
 
         <p className={css.copyright}>
