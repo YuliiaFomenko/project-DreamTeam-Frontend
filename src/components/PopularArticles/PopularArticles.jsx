8321c233d06a3e866415402e698d51a3280caafd
Bohdan Predchuk <predchukbohdan1995@ukr.net>
2025-08-02 21:20:58 +0300

    added popular articles section markup and styles

--- src/components/PopularArticles/PopularArticles.jsx
+++ src/components/PopularArticles/PopularArticles.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import sprite from "../../assets/img/sprite.svg";
+import s from "./PopularArticles.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { selectPopularArticles } from "../../redux/articles/selectors";
+import { useEffect } from "react";
+import { fetchPopular } from "../../redux/articles/operations";
 
 const PopularArticles = () => {
-  return (
-    <div>PopularArticles</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default PopularArticles
+  useEffect(() => {
+    dispatch(fetchPopular());
+  }, [dispatch]);
+  const articles = useSelector(selectPopularArticles);
+  console.log(articles);
+
+  return (
+    <div className={s.popularArticles}>
+      <div className="container">
+        <div className={s.top}>
+          <h2 className={s.title}>Popular Articles</h2>
+          <Link className={s.link} to="/articles">
+            Go to all Articles
+            <svg width="32" height="32" stroke="var(--green-darker)">
+              <use href={`${sprite}#icon-arrows-up`} />
+            </svg>
+          </Link>
+        </div>
+        <ul className={s.grid}>
+          {articles.slice(0, 4).map((article) => (
+            <li key={article._id}></li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default PopularArticles;
