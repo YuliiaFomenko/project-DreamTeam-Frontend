4893653400454770d79772f5a164eb72f7cc2885
Artem-Oganezov <artem_oga@i.ua>
2025-08-02 16:46:25 +0300

    add btn logic

--- src/components/ButtonAddToBookmarks/ButtonAddToBookmarks.jsx
+++ src/components/ButtonAddToBookmarks/ButtonAddToBookmarks.jsx
@@ -6,27 +6,32 @@ import { removeFromSaved, addToSaved } from "../../redux/user/operations.js";
 import { useNavigate } from "react-router-dom";
 import { ModalErrorSave } from "../../components/ModalErrorSave/ModalErrorSave.jsx";
 import { useState } from "react";
+import { refreshThunk } from "../../redux/auth/operations.js";
 
 export default function ButtonOfToBookmarks({ articleId, ownerId }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [showLoginModal, setShowLoginModal] = useState(false);
-  //const isLoggedIn = useSelector(selectIsLoggedIn);
-  const isLoggedIn = true;
+  const [buttonRefresh, setButtonRefresh] = useState(false);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  // const isLoggedIn = true;
   const user = useSelector(selectUser);
-  //const bookmarked = user.savedArticlesIDs.includes(articleId);
-  const bookmarked = false; // For testing purposes, assuming the article is not bookmarked
-  //const ownArticle = user.id === ownerId;
-  const ownArticle = true; // For testing purposes, assuming the user owns the article
+  const bookmarked = user.savedArticlesIDs.includes(articleId);
+  // const bookmarked = false; // For testing purposes, assuming the article is not bookmarked
+  const ownArticle = user.id === ownerId;
+  // const ownArticle = true; // For testing purposes, assuming the user owns the article
 
   const handleEditClick = () => {
     navigate(`/articles/${articleId}`);
   };
   const handleRemoveBookmarkClick = () => {
     dispatch(removeFromSaved(articleId));
+    dispatch(refreshThunk());
   };
   const handleAddBookmarkClick = () => {
     dispatch(addToSaved(articleId));
+    dispatch(refreshThunk());
   };
   const handleOpenModalClick = () => {
     setShowLoginModal(true);
@@ -35,28 +40,28 @@ export default function ButtonOfToBookmarks({ articleId, ownerId }) {
     <>
       {!isLoggedIn && (
         <button className={s.bookmark} onClick={handleOpenModalClick}>
-          <svg width="34" height="34">
+          <svg width="24" height="24">
             <use href={`${sprite}#icon-bookmark-alternative`} />
           </svg>
         </button>
       )}
       {isLoggedIn && bookmarked && !ownArticle && (
         <button className={s.bookmarked} onClick={handleRemoveBookmarkClick}>
-          <svg width="34" height="34">
+          <svg width="24" height="24">
             <use href={`${sprite}#icon-bookmark-alternative`} />
           </svg>
         </button>
       )}
       {isLoggedIn && !bookmarked && !ownArticle && (
         <button className={s.bookmark} onClick={handleAddBookmarkClick}>
-          <svg width="34" height="34">
+          <svg width="24" height="24">
             <use href={`${sprite}#icon-bookmark-alternative`} />
           </svg>
         </button>
       )}
       {isLoggedIn && ownArticle && (
         <button className={s.edit} onClick={handleEditClick}>
-          <svg width="34" height="34">
+          <svg width="24" height="24">
             <use href={`${sprite}#icon-bookmark`} />
           </svg>
         </button>
