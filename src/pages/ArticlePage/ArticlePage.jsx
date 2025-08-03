3cd05435baf50f16d68f30f5502280e94cd18084
Vika <wiktoria.tarasuk@gmail.com>
2025-08-03 13:05:03 +0300

    main commit

--- src/pages/ArticlePage/ArticlePage.jsx
+++ src/pages/ArticlePage/ArticlePage.jsx
@@ -1,43 +1,69 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 
-import { fetchArticleById } from '../../redux/articles/operations';
-import { fetchUserInfo } from '../../redux/user/operations';
-import { selectArticleById, selectArticlesIsLoading } from '../../redux/articles/selectors';
+import { fetchArticleById, fetchRandom } from '../../redux/articles/operations';
+import { addToSaved, fetchUserInfo } from '../../redux/user/operations';
+import { selectArticleById, selectArticlesIsLoading, selectRandomArticles } from '../../redux/articles/selectors';
 import { selectUserInfo, selectUserIsLoading } from '../../redux/user/selectors';
 
 import styles from './ArticlePage.module.css';
 import Loader from '../../components/Loader/Loader';
 import sprite from '../../assets/img/sprite.svg';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
+import { ModalErrorSave } from '../../components/ModalErrorSave/ModalErrorSave';
+import { useBodyLock } from '../../hooks/useBodyLock/useBodyLock';
 
 const ArticlePage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { id } = useParams();
 
-  const articleData = useSelector(state => selectArticleById(state, id));
+  const article = useSelector(state => selectArticleById(state, id));
   const isArticleLoading = useSelector(selectArticlesIsLoading);
-
-  const article = articleData;
 
   const userId =
     typeof article?.ownerId === 'object' && article.ownerId?.$oid
       ? article.ownerId.$oid
       : article?.ownerId;
 
-  const authorResponse = useSelector(state => selectUserInfo(state, userId));
-  const author = authorResponse?.data || authorResponse;
+  const author = useSelector(state => selectUserInfo(state, userId));
   const isAuthorLoading = useSelector(selectUserIsLoading);
+
+  const randomArticles = useSelector(selectRandomArticles);
+  const allAuthors = useSelector(state => state.user.users); 
+
+  const [isModalOpen, setModalOpen] = useState(false);
+  useBodyLock(isModalOpen);
+  const isLogged = useSelector(selectIsLoggedIn);
 
   useEffect(() => {
     dispatch(fetchArticleById(id));
   }, [dispatch, id]);
 
   useEffect(() => {
+    dispatch(fetchRandom());
+  }, [dispatch]);
+
+  useEffect(() => {
     if (userId) {
       dispatch(fetchUserInfo(userId));
     }
   }, [dispatch, userId]);
+
+  useEffect(() => {
+    if (randomArticles.length > 0) {
+      randomArticles.forEach(articleItem => {
+        const ownerId =
+          typeof articleItem?.ownerId === 'object' && articleItem.ownerId?.$oid
+            ? articleItem.ownerId.$oid
+            : articleItem?.ownerId;
+        if (ownerId && !allAuthors?.[ownerId]) {
+          dispatch(fetchUserInfo(ownerId));
+        }
+      });
+    }
+  }, [randomArticles, dispatch, allAuthors]);
 
   if (isArticleLoading) return <Loader />;
   if (!article) return <p className={styles.error}>Article not found</p>;
@@ -49,6 +75,25 @@ const ArticlePage = () => {
         year: 'numeric',
       })
     : '';
+
+  const handleSaveClick = () => {
+    if (!isLogged) {
+      setModalOpen(true);
+      return;
+    }
+
+    dispatch(addToSaved(article._id))
+      .unwrap()
+      .catch((err) => {
+        if (err?.includes('401')) {
+          setModalOpen(true);
+        }
+      });
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
 
   return (
     <div className="container">
@@ -83,35 +128,36 @@ const ArticlePage = () => {
               <div className={styles.recommendation}>
                 <h3 className={styles.recommendationTitle}>You can also interested</h3>
                 <ul className={styles.recommendationList}>
-                  <li className={styles.recommendationItem}>
-                    First link
-                    <button className={styles.btnOpen}>
-                      <svg className={styles.arrow}>
-                        <use href={`${sprite}#icon-arrows-right`} />
-                      </svg>
-                    </button>
-                  </li>
-                  <li className={styles.recommendationItem}>
-                    Second link
-                    <button className={styles.btnOpen}>
-                      <svg className={styles.arrow}>
-                        <use href={`${sprite}#icon-arrows-right`} />
-                      </svg>
-                    </button>
-                  </li>
-                  <li className={styles.recommendationItem}>
-                    Third link
-                    <button className={styles.btnOpen}>
-                      <svg className={styles.arrow}>
-                        <use href={`${sprite}#icon-arrows-right`} />
-                      </svg>
-                    </button>
-                  </li>
+                  {randomArticles.map(item => {
+                    const itemUserId =
+                      typeof item?.ownerId === 'object' && item.ownerId?.$oid
+                        ? item.ownerId.$oid
+                        : item.ownerId;
+
+                    const authorInfo = allAuthors?.[itemUserId];
+
+                    return (
+                      <li key={item._id} className={styles.recommendationItem}>
+                        <span className={styles.recommendationItemText}>
+                          {item.title}
+                          {authorInfo?.name && <div className={styles.recommendationItemAuthor}>{authorInfo.name}</div>}
+                        </span>
+                        <button
+                          className={styles.btnOpen}
+                          onClick={() => navigate(`/articles/${item._id}`)}
+                        >
+                          <svg className={styles.arrow}>
+                            <use href={`${sprite}#icon-arrows-right`} />
+                          </svg>
+                        </button>
+                      </li>
+                    );
+                  })}
                 </ul>
               </div>
             </div>
 
-            <button className={styles.buttonSave}>
+            <button className={styles.buttonSave} onClick={handleSaveClick}>
               Save
               <svg className={styles.icon}>
                 <use href={`${sprite}#icon-bookmark-alternative`} />
@@ -120,6 +166,7 @@ const ArticlePage = () => {
           </div>
         </div>
       </div>
+      {isModalOpen && <ModalErrorSave onClose={handleCloseModal} />}
     </div>
   );
 };
