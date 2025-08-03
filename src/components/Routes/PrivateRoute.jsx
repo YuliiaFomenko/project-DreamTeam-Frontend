15794d586705a2d30769f010bd75de50c51611ce
Yuliia Fomenko <yaulia3@gmail.com>
2025-08-03 19:24:03 +0100

    update Private route

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -3,17 +3,23 @@ import { useSelector } from "react-redux";
 import { selectIsLoggedIn, selectIsRefreshing } from "../../redux/auth/selectors.js";
 import { Navigate } from "react-router-dom";
 import toast from "react-hot-toast";
+import { useRef } from "react";
 
 const PrivateRoute = ({ children }) => {
   const isLoggedUser = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
 
+  const hasShownToast = useRef(false);
+
   if (isRefreshing) {
-    return <Loader />; // або null, або просто нічого не рендерити
+    return <Loader />;
   }
 
   if (!isLoggedUser) {
-    toast.error("You must be logged in to access this page.");
+    if (!hasShownToast.current) {
+      toast.error("You must be logged in to access this page.");
+      hasShownToast.current = true;
+    }
     return <Navigate to="/login" />;
   }
 
