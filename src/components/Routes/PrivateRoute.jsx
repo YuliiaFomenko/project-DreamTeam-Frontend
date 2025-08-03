a0598d249e6b023482532bf905b1aa04229bbe39
Yuliia Fomenko <yaulia3@gmail.com>
2025-08-03 19:55:47 +0100

    back Private Route to the initial view

--- src/components/Routes/PrivateRoute.jsx
+++ src/components/Routes/PrivateRoute.jsx
@@ -1,25 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { selectIsLoggedIn, selectIsRefreshing } from "../../redux/auth/selectors.js";
+import { selectIsLoggedIn} from "../../redux/auth/selectors.js";
 import { Navigate } from "react-router-dom";
 import toast from "react-hot-toast";
-import { useRef } from "react";
+
 
 const PrivateRoute = ({ children }) => {
   const isLoggedUser = useSelector(selectIsLoggedIn);
-  const isRefreshing = useSelector(selectIsRefreshing);
-
-  const hasShownToast = useRef(false);
-
-  if (isRefreshing) {
-    return <Loader />;
-  }
-
+  
   if (!isLoggedUser) {
-    if (!hasShownToast.current) {
-      toast.error("You must be logged in to access this page.");
-      hasShownToast.current = true;
-    }
+    toast.error("You must be logged in to access this page.");
     return <Navigate to="/login" />;
   }
 
